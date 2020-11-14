850909cdd019e306a7d27d1feda3cd73e0fd5b5f
sinseman44 <sinseman44@gmail.com>
2020-11-14 19:07:42 +0000

    correct setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 from setuptools import setup, find_packages
 setup(
     name="pycnc",
-    version="2.0.1",
+    version="2.0.0",
     packages=find_packages(),
     scripts=['pycnc'],
 
